9d114b4677b4dfb4899e179b614681dff80385fd
young1102-ui <ya200809@gmail.com>
2026-09-03 23:47:41 +0900

    Delete test_run_kor.js

--- test_run_kor.js
+++
@@ -1,5 +0,0 @@
-const { englishToKoreanPhonetics } = require('./phonetic.js');
-
-console.log('EN:', 'Korean pronunciation');
-console.log('  Natural:', englishToKoreanPhonetics('Korean pronunciation', 'natural'));
-console.log('  Clear:  ', englishToKoreanPhonetics('Korean pronunciation', 'clear'));
